666aafa6cc8df075b517456ed1bd903caba7e158
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-11-08 20:19:52 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import ProfileEdit from './pages/ProfileEdit';
 import Profile from './pages/Profile';
@@ -7,9 +7,61 @@ import Favorites from './pages/Favorites';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
+import Loading from './component/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      userEmail: '',
+      userImage: '',
+      userDescription: '',
+      loading: false,
+      wasLogged: false,
+      isLoginButtonDisabled: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+
+    const minLength = 3;
+    const inputValue = value.length < minLength;
+
+    this.setState({ [name]: value }, () => {
+      this.setState({ isLoginButtonDisabled: inputValue });
+    });
+  };
+
+  onSubmitButton = async (event) => {
+    event.preventDefault();
+    const { userName, userEmail, userImage, userDescription } = this.state;
+    this.setState({ loading: true }, async () => {
+      await createUser({
+        name: userName,
+        email: userEmail,
+        image: userImage,
+        description: userDescription,
+      });
+      this.setState({
+        loading: false,
+        wasLogged: true,
+      });
+    });
+  };
+
   render() {
+    const {
+      userName,
+      userEmail,
+      userImage,
+      userDescription,
+      loading,
+      wasLogged,
+      isLoginButtonDisabled } = this.state;
+
     return (
       <BrowserRouter>
         <Switch>
@@ -18,7 +70,18 @@ class App extends React.Component {
           <Route path="/profile" render={ () => <Profile /> } />
           <Route path="/favorites" render={ () => <Favorites /> } />
           <Route path="/search" render={ () => <Search /> } />
-          <Route exact path="/" render={ () => <Login /> } />
+          <Route exact path="/">
+            { loading ? <Loading /> : <Login
+              userName={ userName }
+              userEmail={ userEmail }
+              userImage={ userImage }
+              userDescription={ userDescription }
+              isLoginButtonDisabled={ isLoginButtonDisabled }
+              onInputChange={ this.onInputChange }
+              onSubmitButton={ this.onSubmitButton }
+            />}
+            { wasLogged ? <Redirect to="/search" /> : null }
+          </Route>
           <Route path="*" render={ () => <NotFound /> } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const {
+      userName,
+      isLoginButtonDisabled,
+      onInputChange,
+      onSubmitButton } = this.props;
+
     return (
       <div data-testid="page-login">
-        Login
+        <form onSubmit={ onSubmitButton }>
+          <label htmlFor="userName">
+            Nome
+            <input
+              type="text"
+              placeholder="Digite seu nome"
+              data-testid="login-name-input"
+              name="userName"
+              className="userName"
+              id="userName"
+              minLength="3"
+              value={ userName }
+              onChange={ onInputChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            name="login-button"
+            disabled={ isLoginButtonDisabled }
+            onClick={ onSubmitButton }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  userName: PropTypes.string.isRequired,
+  onSubmitButton: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  isLoginButtonDisabled: PropTypes.bool.isRequired,
+};
 
 export default Login;
